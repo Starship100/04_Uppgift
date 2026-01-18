385ecece21f9f2e38d934d913f4d0ccc6131ce37
Starship100 <jimmy.widlund@outlook.com>
2026-01-18 19:59:31 +0100

    1a,1b,1c

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+# 1a
+distance = 470
+speed = int(input("Ange Hastighet i km/h: "))
+#speed_int = int(speed)
+time_in_hours = distance / speed
+print("Det tar " + str(time_in_hours) + "h")
+
+# 1b
+distance = 470
+speed = int(input("Ange Hastighet i km/h: "))
+#speed_int = int(speed)
+time_in_hours = distance / speed
+time_in_minutes = time_in_hours * 60
+print("Det tar " + str(time_in_minutes) + "min")
+
+# 1c
+distance = 470
+speed = int(input("Ange Hastighet i km/h: "))
+minutes = int(distance / speed * 60)
+time_in_hours = minutes // 60
+time_in_minutes = minutes % 60
+print("Det tar " + str(time_in_hours) + " timmar och " + str(time_in_minutes) + " minuter")
